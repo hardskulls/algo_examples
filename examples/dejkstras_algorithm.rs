5867d19d2e40c0f2fe45a93eaac36a6248490e18
hardskulls <hardskulls@rambler.ru>
2023-06-28 19:45:26 +0300

    #[✨style (all)] Updated code structure.

--- examples/dejkstras_algorithm.rs
+++ examples/dejkstras_algorithm.rs
@@ -1,5 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
+use algo_examples::formatting::on_screen_len;
+use algo_examples::shorthands::new_h_map;
+
 type NodeGraph<K, V> = HashMap<K, HashMap<K, V>>;
 
 fn main()
@@ -34,54 +37,21 @@ fn main()
     println!("{border}\n| {print} |\n{border}");
 }
 
-fn _at_idx(idx: usize)
-    -> std::ops::RangeInclusive<usize>
-{ idx..=idx }
-
-fn is_emoji(c: char) -> bool
-{
-    match c
-    {
-        '\u{01F600}'..='\u{01F64F}' |
-        '\u{01F300}'..='\u{01F5FF}' |
-        '\u{01F680}'..='\u{01F6FF}' |
-        '\u{01F1E0}'..='\u{01F1FF}' |
-        '\u{002702}'..='\u{0027B0}' |
-        '\u{0024C2}'..='\u{01F251}' => true,
-        _ => false,
-    }
-}
-
-fn on_screen_len(s: &str) -> usize {
-    let count = |acc, c| if is_emoji(c) { acc + 2 } else { acc + 1 };
-    s.chars().fold(0, count)
-}
-
-fn new_h_map<'a, K, const L: usize>(arr: [(&'a K, i32); L])
-    -> HashMap<&'a K, i32>
-    where
-        K: Eq + Hash + ?Sized,
-{ HashMap::from(arr) }
-
 use std::hash::Hash;
 
-fn find_lowest_cost_node<'a, K: Eq + Hash + ?Sized>
+fn find_lowest_cost_node<'a, K : Eq + Hash + ?Sized>
 (
-    costs: &HashMap<&'a K, i32>,
-    processed: &HashSet<&K>,
+    costs : &HashMap<&'a K, i32>,
+    processed : &HashSet<&K>,
 )
     -> Option<&'a K>
 {
-    let mut lowest_cost = i32::MAX;
-    let mut lowest_cost_node: Option<&K> = None;
+    let (mut lowest_cost, mut lowest_cost_node) = (i32::MAX, None);
 
     for (&node, &cost) in costs
     {
         if !processed.contains(node) && cost < lowest_cost
-        {
-            lowest_cost = cost;
-            lowest_cost_node = Some(node);
-        }
+        { (lowest_cost, lowest_cost_node) = (cost, Some(node)) }
     }
 
     lowest_cost_node
@@ -91,11 +61,11 @@ fn find_lowest_cost_node<'a, K: Eq + Hash + ?Sized>
 /// shortest path in a weighted graph.
 ///
 /// [!!] Cannot be used with negative weights. [!!]
-pub fn dejkstras_alg<K: Eq + Hash + ?Sized>
+pub fn dejkstras_alg<K : Eq + Hash + ?Sized>
 (
-    graph: &NodeGraph<&K, i32>,
-    start: &K,
-    finish: &K,
+    graph : &NodeGraph<&K, i32>,
+    start : &K,
+    finish : &K,
 )
     -> Option<i32>
 {
@@ -132,32 +102,9 @@ mod tests
     use std::time::Duration;
 
     use algo_examples::benchmarking::{bench_once, bench_times, calc_iterations};
+    use algo_examples::formatting::{on_screen_len, is_emoji};
 
     use super::*;
-
-    #[test]
-    fn is_emoji_test()
-    {
-        "✨✅🚧❌"
-            .chars()
-            .for_each(|c| assert_eq!(is_emoji(c), true));
-    }
-
-    #[test]
-    fn emojis_len_and_count_test()
-    {
-        assert_eq!(on_screen_len("✨✅🚧❌"), 8);
-        assert_eq!(on_screen_len("✨ ✅🚧❌"), 9);
-        assert_eq!(on_screen_len("✨ ✅ 🚧❌"), 10);
-        assert_eq!(on_screen_len("✨ ✅ 🚧 ❌"), 11);
-        assert_eq!(on_screen_len("✨ ✅ 🚧 ❌ "), 12);
-
-        let s = "✨ It works! Answer is 6 ✅";
-        assert_eq!(on_screen_len(s), 27);
-
-        let s = "🚧 Oh, shieeet, answer is 6 instead of 5 ❌";
-        assert_eq!(on_screen_len(s), 43);
-    }
 
     #[test]
     fn dejkstras_algorithm_test()
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-pub mod benchmarking 
+pub mod benchmarking
 {
     use std::time::{Duration, Instant};
 
-    /// Measure a function's execution time.
-    /// Measurement is done only once.
+    /// Measures the execution time of a function once and returns the duration.
     #[inline]
     pub fn bench_once<F, T>(f : F) -> Duration
-    where
-        F : FnOnce() -> T,
+        where
+            F : FnOnce() -> T,
     {
         let instant = Instant::now();
         f();
@@ -15,7 +14,9 @@ pub mod benchmarking
     }
 
     /// Measure a function's execution time.
-    /// Measurement is executed 'iterations' times.
+    ///
+    /// 'iterations' defines how many measurements there will be.
+    /// Returns the lowest value, thereby minimizing OS influence on results.
     #[inline]
     pub fn bench_times<F, T>(iterations : u32, mut f : F) -> Option<Duration>
         where
@@ -31,16 +32,82 @@ pub mod benchmarking
         vec.into_iter().min()
     }
 
-    /// Calculates how many iterations of benching is allowed in order to fit
-    /// into the specified time limits.
+    /// Calculates the number of iterations of `bench_once` that can be executed within a specified time limit.
     #[inline]
     pub fn calc_iterations(one_measurement_takes : Duration, desired_time : Duration) -> u32
     {
         let mut div = 1;
         while desired_time / div > one_measurement_takes
         { div *= 10 }
-        // For some reason, testing with unmodified answer takes 2x from..
-        // ..expected time, that's why there is a correction here.
+        // For some reason, testing with an unmodified answer takes twice the expected time;
+        // that's why there is a correction here.
         div / 2
+    }
+}
+
+pub mod shorthands
+{
+    use std::{collections::HashMap, hash::Hash};
+
+    pub fn at_idx(idx : usize) -> std::ops::RangeInclusive<usize>
+    { idx ..= idx }
+
+    pub fn new_h_map<'a, K, const L : usize>(arr : [(&'a K, i32); L])
+        -> HashMap<&'a K, i32>
+        where
+            K : Eq + Hash + ?Sized,
+    { HashMap::from(arr) }
+}
+
+pub mod formatting
+{
+    pub fn is_emoji(c : char) -> bool
+    {
+        match c
+        {
+            '\u{01F600}' ..= '\u{01F64F}' |
+            '\u{01F300}' ..= '\u{01F5FF}' |
+            '\u{01F680}' ..= '\u{01F6FF}' |
+            '\u{01F1E0}' ..= '\u{01F1FF}' |
+            '\u{002702}' ..= '\u{0027B0}' |
+            '\u{0024C2}' ..= '\u{01F251}' => true,
+            _ => false,
+        }
+    }
+
+    pub fn on_screen_len(s : &str) -> usize
+    {
+        let count = |acc, c| acc + if is_emoji(c) { 2 } else { 1 };
+        s.chars().fold(0, count)
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use crate::formatting::{is_emoji, on_screen_len};
+
+    #[test]
+    fn is_emoji_test()
+    {
+        "✨✅🚧❌"
+            .chars()
+            .for_each(|c| assert_eq!(is_emoji(c), true));
+    }
+
+    #[test]
+    fn emojis_len_and_count_test()
+    {
+        assert_eq!(on_screen_len("✨✅🚧❌"), 8);
+        assert_eq!(on_screen_len("✨ ✅🚧❌"), 9);
+        assert_eq!(on_screen_len("✨ ✅ 🚧❌"), 10);
+        assert_eq!(on_screen_len("✨ ✅ 🚧 ❌"), 11);
+        assert_eq!(on_screen_len("✨ ✅ 🚧 ❌ "), 12);
+
+        let s = "✨ It works! Answer is 6 ✅";
+        assert_eq!(on_screen_len(s), 27);
+
+        let s = "🚧 Oh, shieeet, answer is 6 instead of 5 ❌";
+        assert_eq!(on_screen_len(s), 43);
     }
 }
